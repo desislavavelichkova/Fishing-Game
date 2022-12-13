b7227ee4ee820596e07209a20f07346d8503be52
Desislava Velichkova <76597321+desislavavelichkova@users.noreply.github.com>
2022-12-13 15:29:10 +0200

    Add files via upload

---
+++ src/Fishes/Fish.java
@@ -0,0 +1,10 @@
+package Fishes;
+
+public class Fish {
+        private int weight;
+
+        public Fish(int weight) {
+                this.weight = weight;
+        }
+        public String FishInfo(){return "You create new fish with " +weight+" kg.";};
+}
